b11c37e5a90ff664c3b21331d3ca5deccd2aaea0
serlus <serluscasas@gmail.com>
2020-07-14 15:49:09 -0300

    função e atributos

---
+++ programacao_funcional/funcao_como_objeto/atributos.py
@@ -0,0 +1,14 @@
+def dobro(x):
+    """investigar como uma função funciona
+    for p in dir(dobro): Assim investigo quais são seus atributos
+        print(p)
+
+    Assim posso ver os atributos e operações ligadas a função. 
+    __name__: me da o nome da função
+    __module__: o modulo onde esta a função
+    __code__:
+    __code__.co_code: compilação em bit do codigo python gerado pela codigo
+    dis: ajuda a interpretar o q faz o código binario
+    from dis import dis
+    """
+    return x * 2
