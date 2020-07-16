b0a7f47d8eef8b8f873a16ae7b50b530212cbc54
serlus <serluscasas@gmail.com>
2020-07-16 18:49:00 -0300

    trem

---
+++ programacao_funcional/OO/trem.py
@@ -0,0 +1,17 @@
+class Trem:
+    def __init__(self, num_vagoes):
+        self.num_vagoes = num_vagoes
+
+    def __len__(self):
+        return self.num_vagoes
+
+    def __getitem__(self, pos):
+        indice = pos if pos >= 0 else self.num_vagoes + pos
+        if 0 <= indice < self.num_vagoes:
+            # indice 2 -> vagao #3
+            return 'vagao #%s' % (indice + 1)
+        raise IndexError('vagao inexistente %s' % pos)
+
+if __name__ == '__main__':
+    for vagao in Trem(4):
+        print(vagao)
